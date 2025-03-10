9799773c7b139967fcee3adf0e0a43e3dbf6e2bd
Alex000291 <mhy88612345@163.com>
2025-03-10 14:00:26 +0800

    modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://alex000291.github.io/space-tourism-website-main/',
-  base: '/space-tourism-website-main/',
+  base: '/Space-Tourism/',
 });
